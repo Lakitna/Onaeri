665c4be92542d81698c95416bba7fe95b359d809
Lakitna <lakitna@gmail.com>
2017-12-20 13:16:01 +0100

    Helper functions optimisations

--- helper.py
+++ helper.py
@@ -9,20 +9,16 @@ def scale(val, inRange, outRange, decimals=0):
     if val is None:
         return None
 
-    ret = (
+    result = (
         ((val - inRange[0]) / (inRange[1] - inRange[0]))
         * (outRange[1] - outRange[0])
         + outRange[0]
     )
 
-    if ret % 1 == 0:
-        ret = round(ret)
-    elif decimals == 0:
-        ret = round(ret)
+    if decimals == 0 or result % 1 == 0:
+        return round(result)
     else:
-        ret = round(ret, decimals)
-
-    return ret
+        return round(result, decimals)
 
 
 def sequenceResize(source, length):
@@ -48,12 +44,11 @@ def limitTo(val, rnge):
     """
     Limit input value to a given absolute range
     """
-    if val is None:
-        return None
-    if val < rnge[0]:
-        val = rnge[0]
-    if val > rnge[1]:
-        val = rnge[1]
+    if val is not None:
+        if val < rnge[0]:
+            return rnge[0]
+        if val > rnge[1]:
+            return rnge[1]
     return val
 
 
@@ -64,41 +59,36 @@ def timecodeRange(min, max):
     max = timecodeWrap(max)
     min = timecodeWrap(min)
 
-    rnge = [(min, max)]
-
     if max < min:
         rnge = [
             (min, settings.Global.totalDataPoints),
             (0, max)
         ]
 
-    for phase in rnge:
-        if phase[0] == phase[1]:
-            rnge.remove(phase)
+        # Remove range phases where min == max
+        for phase in rnge:
+            if phase[0] == phase[1]:
+                rnge.remove(phase)
 
-    return rnge
+        return rnge
+    else:
+        return [(min, max)]
 
 
 def inRange(val, rnge):
     """
-    Find out if input value is within a given absolute range
+    Is the input value within one or multiple given absolute ranges
     """
-    def do(value, rnge):
-        if rnge[0] <= value <= rnge[1]:
-            return True
-
     if val is None:
         return False
 
-    for r in rnge:
-        if type(r) is tuple or type(r) is list:
-            if len(r) == 2:
-                if do(val, r):
-                    return True
-        else:
-            if do(val, rnge):
+    if type(rnge[0]) is int:
+        if rnge[0] <= val <= rnge[1]:
+            return True
+    else:
+        for r in rnge:
+            if r[0] <= val <= r[1]:
                 return True
-            break
     return False
 
 
@@ -107,7 +97,7 @@ def timecodeWrap(val):
     Wrap the input so that it's always within timecode range.
     """
     if val < 0:
-        val += settings.Global.totalDataPoints
-    elif val > settings.Global.totalDataPoints:
-        val -= settings.Global.totalDataPoints
+        return val + settings.Global.totalDataPoints
+    if val > settings.Global.totalDataPoints:
+        return val - settings.Global.totalDataPoints
     return val
